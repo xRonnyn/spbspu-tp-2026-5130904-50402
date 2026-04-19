aa54ba4dce6c59f7c9564c97bb83fd11cff462be
xRonnyn <pashuha.k@gmail.com>
2026-04-19 21:13:22 +0300

    processing commands in main

--- karpenkov.pavel/T1/main.cpp
+++ karpenkov.pavel/T1/main.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+#include "operations.hpp"
+#include <unordered_map>
 
 int main()
-{}
+{
+  std::unordered_map< std::string, void (*)(std::istream &, std::ostream &, karpenkov::mapOfNotes &) > listOfCommands{
+      {"note", karpenkov::addNote}};
+  karpenkov::mapOfNotes notes;
+  std::string command;
+  while (std::cin >> command) {
+    try {
+      listOfCommands.at(command)(std::cin, std::cout, notes);
+    } catch (...) {
+      std::cout << "<INVALID COMMAND>";
+    }
+  }
+  return 0;
+}
